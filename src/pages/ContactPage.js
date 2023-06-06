3e5079d1d4cc6cf9f4b776884deb9441eaff817c
dodo <cads225@gachon.ac.kr>
2023-06-06 20:45:04 +0900

    feat: 문의하기 페이지 추가

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -2,13 +2,23 @@ import React, { useState, useEffect } from "react";
 import * as cm from "../components/Common";
 import * as sl from "../components/SignupLogin";
 import { Link } from "react-router-dom";
-import Student from "../img/landingpage/student.svg";
 import styled from "styled-components";
-
-import { useLocation } from "react-router-dom";
 const ContactPage = () => {
-  const kakaoLogin = () => {
-    //카카오 로그인 이동 페이지
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [email, setEmail] = useState("");
+
+  const onChangeTitle = (e) => {
+    setTitle(e.target.value);
+    console.log("title");
+  };
+
+  const onChangeContent = (e) => {
+    setContent(e.target.value);
+  };
+
+  const onChangeEmail = (e) => {
+    setEmail(e.target.value);
   };
 
   return (
@@ -25,44 +35,94 @@ const ContactPage = () => {
           <cm.TranslateBtn_white />
         </cm.TopBar_Menu_right>
       </cm.TopBarDark>
-      <Wrapper>
-        <RightWrapper>
-          <StudentImg src={Student} alt="" />
-        </RightWrapper>
-        <LeftWrapper>
-          <sl.Modal>
-            <sl.Modal_header>
-              <cm.BLogo />
-            </sl.Modal_header>
-            <sl.Modal_main></sl.Modal_main>
-            <sl.Modal_footer />
-          </sl.Modal>
-        </LeftWrapper>
-      </Wrapper>
+      <cm.Wrapper>
+        <sl.Modal>
+          <sl.Modal_header>
+            <cm.BLogo />
+          </sl.Modal_header>
+          <sl.Modal_main>
+            <sl.Modal_main_title>문의하기</sl.Modal_main_title>
+
+            <cm.DivHorizon>
+              <sl.Line height="22px" />
+              <GrayText>카페인 팀원에게 문의사항을 보내주세요</GrayText>
+              <sl.Line height="22px" />
+            </cm.DivHorizon>
+            <cm.DivVertical>
+              <sl.InputTitle>제목 *</sl.InputTitle>
+              <sl.InputBox
+                placeholder="제목을 입력하세요"
+                onChange={onChangeTitle}
+              />
+            </cm.DivVertical>
+            <cm.DivVertical>
+              <sl.InputTitle>문의내용 *</sl.InputTitle>
+              <InputBox
+                placeholder="문의사항을 입력하세요"
+                onChange={onChangeContent}
+              ></InputBox>
+            </cm.DivVertical>
+            <cm.DivVertical>
+              <sl.InputTitle>이메일 *</sl.InputTitle>
+              <InputEmail
+                type="email"
+                placeholder="이메일을 입력하세요"
+                onChange={onChangeEmail}
+              />
+            </cm.DivVertical>
+            <cm.BtnBlue className="cancel">문의하기</cm.BtnBlue>
+          </sl.Modal_main>
+          <sl.Modal_footer />
+        </sl.Modal>
+      </cm.Wrapper>
     </>
   );
 };
 
 export default ContactPage;
 
-const Wrapper = styled.div`
-  width: 100vw;
-  height: 93vh;
+export const InputBox = styled.textarea`
+  border-radius: 8px;
+  height: 150px;
+  background-color: #ffffff;
+  border: 1px solid #dae0e6;
+  box-sizing: border-box;
+  font-weight: 400;
+  font-size: 14px;
+  padding-left: 10px;
+  padding-top: 10px;
+  outline: none;
+  resize: none; /* Prevent resizing */
+
   display: flex;
+  align-items: flex-start;
+  &:focus {
+    border-color: black;
+  }
 `;
 
-const RightWrapper = styled.div`
+export const InputEmail = styled.input`
   width: 100%;
-  height: 100%;
-`;
+  height: 40px;
+  border-radius: 8px;
+  background-color: #ffffff;
+  border: 1px solid #dae0e6;
+  box-sizing: border-box;
+  font-weight: 400;
+  font-size: 14px;
+  padding-left: 10px;
+  outline: none;
+  &:focus {
+    border-color: black;
+  }
 
-const LeftWrapper = styled.div`
-  width: 100%;
-  height: 100%;
-  background-color: white;
+  margin-bottom: 20px;
 `;
-const StudentImg = styled.img`
-  width: 100%;
-  height: 100%;
-  margin-left: 50%;
+export const GrayText = styled.p`
+  color: hsla(213, 14%, 43%, 1);
+  font-family: "Poppins";
+  font-weight: lighter;
+  font-size: 13px;
+  margin-top: 10px;
+  padding-top: 5px;
 `;
